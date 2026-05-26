4753e58618ce65610e5eab5492f2d29e1d311cca
Z User <z@container>
2026-05-26 10:41:46 +0000

    fix: remove unused supabase_flutter import in profile_screen
    Zero errors, zero warnings in flutter analyze

--- Daxelo-Kinrel-App/lib/features/profile/presentation/profile_screen.dart
+++ Daxelo-Kinrel-App/lib/features/profile/presentation/profile_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../core/constants/brand_colors.dart';
 import '../../../core/constants/brand_typography.dart';
