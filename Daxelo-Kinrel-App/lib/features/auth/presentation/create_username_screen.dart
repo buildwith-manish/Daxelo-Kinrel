9b852209fad7adaa973efd19c31704ac98046f10
buildwith-manish <manish@buildwith-manish.github>
2026-07-06 12:54:30 +0000

    fix: add supabase_flutter import for UserAttributes in CreateUsernameScreen

--- Daxelo-Kinrel-App/lib/features/auth/presentation/create_username_screen.dart
+++ Daxelo-Kinrel-App/lib/features/auth/presentation/create_username_screen.dart
@@ -10,6 +10,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../core/constants/brand_colors.dart';
 import '../../../core/constants/brand_typography.dart';
